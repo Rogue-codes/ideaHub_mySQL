4a098424d87ed3597a14a1246359cd61e22344d4
Vermax <osujinnamdidaniel@OSUJIs-MacBook-Pro.local>
2023-09-19 02:49:25 +0100

    update hub endpoin

--- src/controllers/hub/index.js
+++ src/controllers/hub/index.js
@@ -51,59 +51,98 @@ export const createHub = (req, res) => {
 
 // get hubs
 export const getHubs = (req, res) => {
-    const q = "SELECT * FROM hub WHERE admin_id = ?"
+  const q = "SELECT * FROM hub WHERE admin_id = ?";
 
-    const admin = req.admin
-    const admin_id = admin.admin_id
-    db.query(q, [admin_id],(err,data)=>{
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          message: err.message
-        })
-      }
-      res.status(200).json({
-        success: true,
-        message:"hubs retrieved successfully",
-        data
-      })
-    })
-}
-// get hub
-export const getHub = (req, res) => {
-  const {id} = req.params
-  if(!id){
-    return res.status(400).json({
-      success:false,
-      message:"hub ID is required"
-    })
-  }
-  const q = "SELECT * FROM hub WHERE admin_id = ? AND hub_id = ?"
-
-  const admin = req.admin
-  const admin_id = admin.admin_id
-  db.query(q, [admin_id,id],(err,data)=>{
+  const admin = req.admin;
+  const admin_id = admin.admin_id;
+  db.query(q, [admin_id], (err, data) => {
     if (err) {
       return res.status(500).json({
         success: false,
-        message: err.message
-      })
-    }
-    if(!data.length > 0){
-      return res.status(404).json({
-        success:false,
-        message:"hub not found"
-      })
+        message: err.message,
+      });
     }
     res.status(200).json({
       success: true,
-      message:"hub retrieved successfully",
-      data
-    })
-  })
-}
+      message: "hubs retrieved successfully",
+      data,
+    });
+  });
+};
+// get hub
+export const getHub = (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({
+      success: false,
+      message: "hub ID is required",
+    });
+  }
+  const q = "SELECT * FROM hub WHERE admin_id = ? AND hub_id = ?";
+
+  const admin = req.admin;
+  const admin_id = admin.admin_id;
+  db.query(q, [admin_id, id], (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+    if (!data.length > 0) {
+      return res.status(404).json({
+        success: false,
+        message: "hub not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "hub retrieved successfully",
+      data,
+    });
+  });
+};
 
 // update hub
+export const updateHub = (req, res) => {
+  const { hub_name, description } = req.body;
+  const hubId = req.params.id; // Corrected variable name
+
+  if (!hubId) {
+    return res.status(400).json({
+      success: false,
+      message: "Hub ID is required",
+    });
+  }
+
+  const adminId = req.admin.admin_id; // Simplified variable assignment
+
+  // Build the SQL query for updating the hub
+  const q = "UPDATE hub SET hub_name = ?, description = ? WHERE admin_id = ? AND hub_id = ?";
+
+  // Execute the update query
+  db.query(q, [hub_name, description, adminId, hubId], (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+
+    if (data.affectedRows === 0) { // Checking affectedRows for successful update
+      return res.status(404).json({
+        success: false,
+        message: "Hub not found or you don't have permission to update it",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Hub updated successfully",
+      data,
+    });
+  });
+};
 
 
 // delete hub
--- src/routes/hubRoute/index.js
+++ src/routes/hubRoute/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { adminMiddleware } from '../../middleware/adminMiddleware.js'
-import { createHub, getHub, getHubs } from '../../controllers/hub/index.js'
+import { createHub, getHub, getHubs, updateHub } from '../../controllers/hub/index.js'
 
 const hubRoute = express.Router()
 
 hubRoute.post('/hub/create', adminMiddleware, createHub)
 hubRoute.get('/hubs/all', adminMiddleware, getHubs)
 hubRoute.get('/hubs/:id', adminMiddleware, getHub)
+hubRoute.put('/hubs/update/:id', adminMiddleware, updateHub)
 
 export default hubRoute
